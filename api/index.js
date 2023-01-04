f5f0ab4587fa01c65f14c6a1652af9a5511b5725
overcarl <121311427+overcarl@users.noreply.github.com>
2023-01-04 22:01:05 +0000

    - added new strings - added login

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
+const passport = require('passport')
 const app = express.Router();
 
 app.get("/", (req,res) => {
   res.json({
     api: "working"
   })
+})
+
+app.get("/auth/callback", passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { 
+  res.redirect("/");
+})
+
+app.get("/user/info", (req, res) => {
+  res.json(req.user||undefined)
 })
 
 module.exports = app;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const DiscordStrategy = require('passport-discord').Strategy;
+const Strategy = require('passport-discord').Strategy;
 const session = require('express-session');
 const i18next = require('i18next');
 const Backend = require('i18next-fs-backend');
@@ -11,16 +11,16 @@ const fs = require('node:fs');
 const expressLayouts = require('express-ejs-layouts')
 
 i18next
-.use(Backend)
-.use(i18nextMiddleware.LanguageDetector)
-.init({
-  backend: {
-    loadPath: __dirname + '/src/locale/{{lng}}/{{ns}}.json'
-  },
-  fallbackLng: 'en',
-  preload: ['en'],
-  ns: ['common', '404', '500']
-});
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    backend: {
+      loadPath: __dirname + '/src/locale/{{lng}}/{{ns}}.json'
+    },
+    fallbackLng: 'en',
+    preload: ['en', 'pt-BR'],
+    ns: ['common', '404', '500']
+  });
 
 app.use(i18nextMiddleware.handle(i18next));
 
@@ -34,6 +34,35 @@ app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 app.use(expressLayouts)
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new Strategy({
+  clientID: process.env.ClientID,
+  clientSecret: process.env.ClientSecret,
+  callbackURL: "/api/auth/callback",
+  scope: [
+    "identify"
+  ]
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    return done(null, profile);
+  })
+}))
+
+app.use(session({
+  secret: "PinwheelSocialIsCool",
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+  
 app.use("/", require('./api/home'))
 app.use("/api", require('./api/index'))
 
